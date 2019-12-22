e59ea4edaf31132e9bce402f983fc748af71ad0d
Alex Gooding <alexxgooding@gmail.com>
2019-12-22 23:59:56 +0000

    create method that will group file paths by folder

--- audio_folder.py
+++ audio_folder.py
@@ -18,3 +18,17 @@ class AudioFolder:
             path_list.extend(glob.glob(pattern, recursive=True))
 
         return path_list
+
+    def get_file_paths_by_folder(self):
+        file_paths = self.get_file_paths()
+        file_paths_by_folder = []
+        while file_paths:
+            path_to_check = file_paths.pop(0)
+            folder_paths = [path_to_check]
+            for path in file_paths:
+                if path.split('\\')[-2] == path_to_check.split('\\')[-2]:
+                    folder_paths.append(path)
+            file_paths_by_folder.append(folder_paths)
+            file_paths = [path for path in file_paths if path not in folder_paths]
+
+        return file_paths_by_folder
